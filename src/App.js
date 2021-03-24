0c3fbbaef838ae8c0179aa143fcfeeba8767f615
Dylan <68516422+DGWebDevelopment@users.noreply.github.com>
2021-03-24 23:45:29 +0000

    making changes

--- src/App.js
+++ src/App.js
@@ -5,13 +5,19 @@ import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
 import { Auth } from 'aws-amplify'
 import backgroundImage from "./images/AdobeStock_306938507.jpeg";
 
+import {API, graphqlOperation} from 'aws-amplify'
+import {getUser} from './graphql/queries'
+import {createUser, updateUser} from './graphql/mutations'
+
 
 function App(){
 
   const [premierLeagueStandings, fillStandings]=useState([])
+  const [hasSetTeam, setTeam]=    useState(1)
 
   useEffect(
     () => {
+      console.log("initial effect")
       fetchStandings()
       //fetchUserDetails()
     }, []
@@ -19,8 +25,8 @@ function App(){
 
   async function fetchUserDetails(){
     // 1) Fetch to see if the user has already selected their favourite team (do this by scanning the database for their username and seeing their chosen team)
-    console.log((await(Auth.currentUserInfo())).username)
-
+    const userName = await(Auth.currentUserInfo()).username
+    
 
   }
 
@@ -42,33 +48,52 @@ function App(){
     })
   }
 
-  return (
-    <div style={{textAlign:'center', display:'block'}}>
-      <h1>See the standings for the Premier League below!</h1>
-      <img src={backgroundImage} style={{width:'500px'}} />
-      
+  if(hasSetTeam){
+    return (
+      <div style={{textAlign:'center', display:'block'}}>
+                <h1>See the standings for the Premier League below!</h1>
+                <img src={backgroundImage} style={{width:'500px'}} />
+    
+                <table style={{marginLeft:'auto', marginRight:'auto', position:'relative', top:'25px'}}>
+                  <tbody>
+                    <tr>
+                      <th>Standing:</th><th>Team:</th><th>Points:</th>
+                    </tr>
+                    
+                    {premierLeagueStandings.map(standing=>{
+                      return (
+                        <tr>
+                          <td>{standing.rank}</td><td>{standing.teamName}</td><td>{standing.points}</td>
+                          <td></td>
+                        </tr>
+                      )
+                    })
+                    }
+                  </tbody>
+                </table>
+                <button onClick={()=>{clickityClick(click+1)}}>Change team</button>
 
-      <table style={{marginLeft:'auto', marginRight:'auto', position:'relative', top:'25px'}}>
-        <tbody>
-          <tr>
-            <th>Standing:</th><th>Team:</th><th>Points:</th>
-          </tr>
-          
-          {premierLeagueStandings.map(standing=>{
-            return (
-              <tr>
-                <td>{standing.rank}</td><td>{standing.teamName}</td><td>{standing.points}</td>
-                <td></td>
-              </tr>
-            )
-          })
-          }
-        </tbody>
-      </table>
+                <AmplifySignOut style={{position:'relative',top:'82px'}}/>
+      </div>
+    )
+  }
+  else {
+    return (
+      <div>
+        <h1>Please choose team out of the selection below</h1>
 
-      <AmplifySignOut style={{position:'relative',top:'82px'}}/>
-  </div>
-  )
+
+
+
+
+
+
+
+      </div>
+    )
+  }
 }
+  
+
 
 export default withAuthenticator(App);
